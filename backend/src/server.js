68a37058bd593a793f6005bfadb78f65ca3f102d
tnbcodesvishal <mr.vishalsingh987@gmail.com>
2026-02-24 11:22:43 +0530

    Fix Express 5 breaking change PathError for catch-all route

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === 'production') {
     const frontendDistPath = path.join(__dirname, '../../frontend/dist');
     app.use(express.static(frontendDistPath));
 
-    app.get('*', (req, res) => {
+    app.get(/(.*)/, (req, res) => {
         res.sendFile(path.join(frontendDistPath, 'index.html'));
     });
 } else {
